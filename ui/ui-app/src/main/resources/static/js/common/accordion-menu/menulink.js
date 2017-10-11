93b6854ff42503b7ee095fd33e7c8d3e4835117c
Germán Oñate <german.onate@endeev.com>
2017-10-11 14:26:30 -0300

    Added icons on main sections of Kylo

--- ui/ui-app/src/main/resources/static/js/common/accordion-menu/menulink.js
+++ ui/ui-app/src/main/resources/static/js/common/accordion-menu/menulink.js
@@ -1,16 +1,16 @@
-define(['angular','common/module-name'], function (angular,moduleName) {
+define(['angular', 'common/module-name'], function (angular, moduleName) {
     'use strict';
 
     angular.module(moduleName)
         .run(['$templateCache', function ($templateCache) {
             $templateCache.put('menu-link.tmpl.html',
-            '<md-button flex ui-sref="{{section.sref}}" ng-click="selectMenuItem()" class="nav-btn" \n ' +
-            'ng-class="{\'selected\' : section.selected,\'md-icon-button\': controller.collapsed}"  > \n' +
-            '<div class="layout-padding-left-8 menu-link"><ng-md-icon md-icon icon="{{section.icon}}" class="nav-btn" ng-class="{\'selected\' : section.selected }" ></ng-md-icon> \n '+
-            '<md-tooltip md-direction="right" ng-if="controller.collapsed" >{{section.text}}</md-tooltip>'+
-            '<span style="padding-left:10px;" ng-if="!controller.collapsed">{{section.text}}</span> '
-            + '</div>'
-            + '</md-button>\n '+
+                '<md-button flex ui-sref="{{section.sref}}" ng-click="selectMenuItem()" class="nav-btn" \n ' +
+                'ng-class="{\'selected\' : section.selected,\'md-icon-button\': controller.collapsed}"  > \n' +
+                '<div class="layout-padding-left-8 menu-link"><ng-md-icon md-icon icon="{{section.icon}}" class="nav-btn" ng-class="{\'selected\' : section.selected }" ></ng-md-icon> \n ' +
+                '<md-tooltip md-direction="right" ng-if="controller.collapsed" >{{section.text}}</md-tooltip>' +
+                '<span style="padding-left:10px;" ng-if="!controller.collapsed">{{section.text}}</span> '
+                + '</div>'
+                + '</md-button>\n ' +
                 '');
         }])
         .directive('menuLink', function () {
@@ -20,7 +20,7 @@ define(['angular','common/module-name'], function (angular,moduleName) {
                 },
                 require: '^accordionMenu',
                 templateUrl: 'menu-link.tmpl.html',
-                link: function ($scope, $element,attrs,controller) {
+                link: function ($scope, $element, attrs, controller) {
 
                     $scope.controller = controller;
 
@@ -30,11 +30,11 @@ define(['angular','common/module-name'], function (angular,moduleName) {
                         controller.autoFocusContent = true;
 
                     };
-                    $scope.isCollapsed = function(){
+                    $scope.isCollapsed = function () {
                         return controller.isCollapsed();
                     }
-                    $scope.sectionClass = function() {
-                        if($scope.section == controller.currentSection ) {
+                    $scope.sectionClass = function () {
+                        if ($scope.section == controller.currentSection) {
                             return 'selected';
                         }
                         else {
